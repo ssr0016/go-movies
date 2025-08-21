fe34c7534e83d4c6b1f49bcccd2222706486486b
ssr0016 <samsonrecluta0016@gmail.com>
2025-08-21 21:51:21 +0800

    connecting the front end to the back end API

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,34 +5,20 @@ const Movies = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    let moviesList = [
-      {
-        id: 1,
-        title: "Highlander",
-        release_date: "1986-03-07",
-        runtime: 116,
-        mpaa_rating: "R",
-        description: "Some long description",
-      },
-      {
-        id: 2,
-        title: "The Matrix",
-        release_date: "1999-03-30",
-        runtime: 136,
-        mpaa_rating: "R",
-        description: "A science fiction action film",
-      },
-      {
-        id: 3,
-        title: "Inception",
-        release_date: "2010-07-16",
-        runtime: 148,
-        mpaa_rating: "PG-13",
-        description: "A mind-bending thriller",
-      },
-    ];
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
 
-    setMovies(moviesList);
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
+    };
+
+    fetch(`http://localhost:8080/movies`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   return (

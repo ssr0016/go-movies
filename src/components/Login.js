3ba1f14a77e3ba399d2ae1740e6162584c48a9ee
ssr0016 <samsonrecluta0016@gmail.com>
2025-08-21 20:21:58 +0800

    creating the login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,40 @@
+import React, { useState } from "react";
+import Input from "./form/Input";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
+    <div className="col-md-6 offset-md-3">
       <h2>Login</h2>
       <hr />
+
+      <form onSubmit={handleSubmit}>
+        <Input
+          title="Email Address"
+          type="email"
+          className="form-control"
+          name="email"
+          placeholder="Enter your email"
+          autoComplete="email-new"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input
+          title="Password"
+          type="password"
+          className="form-control"
+          name="password"
+          placeholder="Enter your email"
+          autoComplete="password-new"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <hr />
+        <Input type="submit" className="btn btn-primary" value="Login" />
+      </form>
     </div>
   );
 };

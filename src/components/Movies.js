8e4a1eb1dc30895bdd0c2bebfe6ce78dc6554a3a
ssr0016 <samsonrecluta0016@gmail.com>
2025-08-21 19:39:23 +0800

    working on the movies component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,64 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    let moviesList = [
+      {
+        id: 1,
+        title: "Highlander",
+        release_date: "1986-03-07",
+        runtime: 116,
+        mpaa_rating: "R",
+        description: "Some long description",
+      },
+      {
+        id: 2,
+        title: "The Matrix",
+        release_date: "1999-03-30",
+        runtime: 136,
+        mpaa_rating: "R",
+        description: "A science fiction action film",
+      },
+      {
+        id: 3,
+        title: "Inception",
+        release_date: "2010-07-16",
+        runtime: 148,
+        mpaa_rating: "PG-13",
+        description: "A mind-bending thriller",
+      },
+    ];
+
+    setMovies(moviesList);
+  }, []);
+
   return (
     <div>
       <h2>Movies</h2>
       <hr />
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Movie</th>
+            <th>Release Date</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((m) => (
+            <tr key={m.id}>
+              <td>
+                <Link to={`/movies/${m.id}`}>{m.title}</Link>
+              </td>
+              <td>{m.release_date}</td>
+              <td>{m.mpaa_rating}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };

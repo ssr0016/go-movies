4b593e131e92972ce49ff7669f7a4342e179ea83
ssr0016 <samsonrecluta0016@gmail.com>
2025-08-21 19:42:02 +0800

    routing from the movies component to individual movies

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import EditMovie from "./components/EditMovie";
 import ManageCatalogue from "./components/ManageCatalogue";
 import GraphQL from "./components/GraphQL";
 import Login from "./components/Login";
+import Movie from "./components/Movie";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: "/movies",
         element: <Movies />,
+      },
+      {
+        path: "/movies/:id",
+        element: <Movie />,
       },
       {
         path: "/genres",

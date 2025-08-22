b393d41b47856232cc7458f0e2e3ba65332497e2
ssr0016 <samsonrecluta0016@gmail.com>
2025-08-23 00:19:42 +0800

    working refresh token

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import Alert from "./components/Alert";
 
@@ -6,6 +6,8 @@ function App() {
   const [jwtToken, setJwtToken] = useState("");
   const [alertMessage, setAlertMessage] = useState("");
   const [alertClassName, setAlertClassName] = useState("d-none");
+
+  const [tickInterval, setTickInterval] = useState();
 
   const navigate = useNavigate();
 
@@ -20,10 +22,46 @@ function App() {
       })
       .finally(() => {
         setJwtToken("");
+        toggleRefresh(false);
       });
 
     navigate("/login");
   };
+
+  const toggleRefresh = useCallback(
+    (status) => {
+      console.log("clicked");
+
+      if (status) {
+        console.log("turning on ticking");
+        let i = setInterval(() => {
+          const requestOptions = {
+            method: "GET",
+            credentials: "include",
+          };
+
+          fetch(`/refresh`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.access_token) {
+                setJwtToken(data.access_token);
+              }
+            })
+            .catch((error) => {
+              console.log("user is not logged in");
+            });
+        }, 600000);
+        setTickInterval(i);
+        console.log("setting tick interval to", i);
+      } else {
+        console.log("turning off ticking");
+        clearInterval("turning off tickInterval", tickInterval);
+        setTickInterval(null);
+        clearInterval(tickInterval);
+      }
+    },
+    [tickInterval]
+  );
 
   useEffect(() => {
     if (jwtToken === "") {
@@ -37,13 +75,14 @@ function App() {
         .then((data) => {
           if (data.access_token) {
             setJwtToken(data.access_token);
+            toggleRefresh(true);
           }
         })
         .catch((error) => {
           console.log("user is not logged in", error);
         });
     }
-  }, [jwtToken]);
+  }, [jwtToken, toggleRefresh]);
 
   return (
     <div className="container">
@@ -117,6 +156,7 @@ function App() {
               setJwtToken,
               setAlertClassName,
               setAlertMessage,
+              toggleRefresh,
             }}
           />
         </div>
